12cde642d89aa873e6e3fa2218b9130b87b42876
Joao Vitor <dadasjv@hotmail.com>
2025-06-03 01:56:32 -0300

    weebhook clerk

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -1,6 +1,4 @@
-// src/app/api/clerk-webhook/route.ts
 import { Webhook } from 'svix';
-import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { db } from '@/db';
 import { userTypes, users } from '@/db/schema';
@@ -17,11 +15,10 @@ type ClerkUserCreatedEvent = {
 
 export async function POST(req: Request) {
   const payload = await req.text();
-  const headerList = headers();
 
-  const svixId = headerList.get('svix-id')!;
-  const svixTimestamp = headerList.get('svix-timestamp')!;
-  const svixSignature = headerList.get('svix-signature')!;
+  const svixId = req.headers.get('svix-id')!;
+  const svixTimestamp = req.headers.get('svix-timestamp')!;
+  const svixSignature = req.headers.get('svix-signature')!;
 
   const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET!);
   let evt: ClerkUserCreatedEvent;
